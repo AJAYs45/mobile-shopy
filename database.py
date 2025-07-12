5f58913cc5a4d2b68b67cbc689c91c388af5cf87
AJAYs45 <ajaysavare45@gmail.com>
2025-07-12 14:59:00 +0530

    ✅ fix: SSL handshake error with TLS config

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
-# database.py
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
+
 MONGO_URL = os.getenv("MONGO_URL")
 
-# SSL handshake issue fix
-client = MongoClient(MONGO_URL, tls=True, tlsAllowInvalidCertificates=True)
+client = MongoClient(
+    MONGO_URL,
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
+
 db = client["mobile_shopy"]
