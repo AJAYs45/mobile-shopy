346b4779717a29bc5313297c2ccc703071c3fbf0
AJAYs45 <ajaysavare45@gmail.com>
2025-07-12 15:13:34 +0530

    🔒 fix: use certifi CA file for secure MongoDB SSL connection

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import certifi
 
 load_dotenv()
 
@@ -9,7 +10,8 @@ MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(
     MONGO_URL,
     tls=True,
-    tlsAllowInvalidCertificates=True
+    tlsCAFile=certifi.where(),
+    tlsAllowInvalidCertificates=False
 )
 
 db = client["mobile_shopy"]
